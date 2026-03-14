06c696a9caf0190986210c32985a9a4f7bb8da9a
C-star-ship <cdotrong098@gmail.com>
2026-03-14 19:17:50 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,23 +3,22 @@ async fetch(request, env) {
 
 const url = new URL(request.url)
 
-if(url.pathname === "/get"){
+if(url.pathname === "/key"){
 
 const ip = request.headers.get("CF-Connecting-IP")
+const device = request.headers.get("User-Agent")
 
-const device = url.searchParams.get("device")
-
-if(!device) return new Response("no device")
-
-const id = ip+"-"+device
+const id = ip + device
 
 let saved = await env.KEYS.get(id)
 
-if(saved) return new Response(saved)
+if(saved){
+return new Response(saved)
+}
 
-let expire = Date.now()+86400000
+let expire = Date.now() + 86400000
 
-let key = "FREE-"+Math.random().toString(36).substring(2,10)+"-"+expire
+let key = "FREE-" + Math.random().toString(36).substring(2,10).toUpperCase() + "-" + expire
 
 await env.KEYS.put(id,key,{expirationTtl:86400})
 
@@ -27,5 +26,6 @@ return new Response(key)
 
 }
 
+return new Response("Key API")
 }
 }
